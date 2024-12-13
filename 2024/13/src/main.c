1633e8e5317d7368388a136b21d29e781c6b2de2
Jakub Antonín Štigler <xstigl00@stud.fit.vut.cz>
2024-12-13 09:34:22 +0100

    Simplify

--- 2024/13/src/main.c
+++ 2024/13/src/main.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <stdbool.h>
-#include <tgmath.h>
 
 typedef struct {
     long long ax;
@@ -30,24 +28,14 @@ int main(void) {
 }
 
 bool read_machine(Machine *m) {
-    int r = scanf(" Button A: X%lld, Y%lld", &m->ax, &m->ay);
+    auto r = scanf(" Button A: X%lld, Y%lld", &m->ax, &m->ay);
     r += scanf(" Button B: X%lld, Y%lld", &m->bx, &m->by);
     r += scanf(" Prize: X=%lld, Y=%lld", &m->px, &m->py);
     return r == 6;
 }
 
 bool solve_machine(Machine *m, long long *a, long long *b) {
-    if (m->ay != 0) {
-        long long n = m->ax;
-        m->ax *= m->ay;
-        m->bx *= m->ay;
-        m->px *= m->ay;
-        m->ay = 0;
-        m->by = m->by * n - m->bx;
-        m->py = m->py * n - m->px;
-    }
-
-    *b = m->py / m->by;
+    *b = (m->py * m->ax - m->px * m->ay) / (m->by * m->ax - m->bx * m->ay);
     *a = (m->px - *b * m->bx) / m->ax;
 
     return *a * m->ax + *b * m->bx == m->px
